358100738cf7fcf55a1d81dde3d6fbe9aa9c4ad1
jhbean130 <jhbean130@gmail.com>
2025-02-22 16:30:08 -0500

    Finished up basic front-end

--- scripts/content.js
+++ scripts/content.js
@@ -1,8 +1,14 @@
+console.log("Content script loaded!");
+
 // Extract the entire HTML from the given webpage
 const pageHTML = document.documentElement.outerHTML
+console.log("Extracted HTML:", pageHTML.substring(0, 100));
 
 // Send the HTML to the background script
-chrome.runtime.sendMessage({
-    action: "sendHTML",
-    html: pageHTML
+chrome.runtime.sendMessage({ action: "sendHTML", html: pageHTML }, (response) => {
+    if (chrome.runtime.lastError) {
+        console.error("Error sending message:", chrome.runtime.lastError);
+    } else {
+        console.log("Message sent successfully", response);
+    }
 });
