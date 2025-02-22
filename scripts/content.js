b1c539a9424034fea7a84635a5dfb69fde891d84
jhbean130 <jhbean130@gmail.com>
2025-02-22 11:52:43 -0500

    Updates to front-end structure and format

---
+++ scripts/content.js
@@ -0,0 +1,9 @@
+// Obtain HTML document from current webpage
+const html = document.getElementsByTagName("html");
+
+// Check to see if HTML was read in correctly
+if (article) {
+
+} else {
+    console.log("Error Printing Out Information")
+}
